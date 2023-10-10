ee2011b5a3baf77344a3dd7d9907212fdb5cb2de
piyush <piyushchandra9458@gmail.com>
2023-10-09 21:14:15 -1200

    changes

--- src/Part.jsx
+++ src/Part.jsx
@@ -1,5 +1,6 @@
 import { useState,useRef } from "react";
 import App from "./App";
+import "./part.css"
 
 
 
@@ -16,9 +17,10 @@ const Part=(()=>{
     
     return(
         <>
-           <div className="relative h-screen flex items-center justify-center text-center bg-center bg-cover transition-opacity  duration-1000" style={{ backgroundImage: `url("/images/image1.jpg")` }}>
+        
+           <div className=" image relative h-screen flex items-center justify-center text-center bg-center bg-cover transition-opacity duration-1000">
       
-            <div className="relative ">
+            <div>
                 <h1 className=" text-5xl font-bold text-white animate-bounce">Workout Warriors Welcome</h1>
                 <p className=" text-3xl mt-3 text-white ">Exercise is a celebration of what your body can do, not a punishment for what you ate</p>
        <button className=" bg-yellow-500 p-3 m-2  rounded-3xl hover:bg-red-600 hover:text-white" onClick={() => changePart('chest')}>chest excercise</button>
@@ -33,6 +35,7 @@ const Part=(()=>{
        <button className="  bg-yellow-500 p-3 m-2 rounded-3xl hover:bg-red-600 hover:text-white" onClick={() => changePart('upper legs')}>upper legs excercise</button>
             </div>
            </div>
+
         <div ref={targetRef}><App  part={part}/></div>
            
         </>
